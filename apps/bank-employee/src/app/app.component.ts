d62825563d918f66297eb553b969adbc9d06eab5
alyoneek <alyonta03@mail.ru>
2024-03-03 17:07:27 +0700

    add taiga ui dependency

--- apps/bank-employee/src/app/app.component.ts
+++ apps/bank-employee/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  TUI_SANITIZER,
+  TuiAlertModule,
+  TuiDialogModule,
+  TuiRootModule,
+  TuiSvgModule,
+} from '@taiga-ui/core';
+import { TuiInputModule } from '@taiga-ui/kit';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiInputModule,
+    TuiSvgModule,
+  ],
   selector: 'bank-employee-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
 export class AppComponent {
   title = 'bank-employee';
--- apps/bank-employee/src/app/app.config.ts
+++ apps/bank-employee/src/app/app.config.ts
@@ -1,7 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { TuiRootModule } from '@taiga-ui/core';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideAnimations(),
+    provideRouter(appRoutes),
+    provideClientHydration(),
+    importProvidersFrom(TuiRootModule),
+  ],
 };
